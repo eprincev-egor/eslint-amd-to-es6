eea0c46a98ea89738bf0d4a36568fadbcedd65f5
eprincev.egor <eprincev.egor@ya.ru>
2020-01-24 11:35:43 +0300

    first commit, tested cases:
    invalid:
    define(['x' + 1], function(x) {})
    define(['x'], function({n}) {})
    define([], function() {console.log(\"test\");})
    define([], () => {console.log(\"test\");})
    
    valid:
    import x from \"x\"

---
+++ tests/lib/rules/amd-to-es6.js
@@ -0,0 +1,79 @@
+"use strict";
+
+let rule = require("../../../lib/rules/amd-to-es6");
+let RuleTester = require("eslint").RuleTester;
+
+let ruleTester = new RuleTester({ 
+    parserOptions: { 
+        ecmaVersion: 2018, 
+        sourceType: "module" 
+    } 
+});
+
+ruleTester.run("amd-to-es6", rule, {
+
+    valid: [
+        {
+            code: "import x from \"x\""
+        }
+    ],
+
+    invalid: [
+        {
+            code: "define(['x' + 1], function(x) {})",
+            errors: [
+                { messageId: "amdPathsShouldBeStrings" }
+            ]
+        },
+        {
+            code: "define(['x'], function({n}) {})",
+            errors: [
+                { messageId: "amdArgumentsShouldBeVariables" }
+            ]
+        },
+        {
+            code: `define([
+                'funcs',
+                'Rows'
+            ], function(f, Rows) {
+                f.some(Rows);
+            })`,
+            errors: [
+                { messageId: "amdNotAllowed" }
+            ],
+            output: [
+                "import f from \"funcs\";\n",
+                "import Rows from \"Rows\";\n",
+                "f.some(Rows);"
+            ].join("")
+        },
+        {
+            code: `define([
+                "funcs"
+            ], function(f) {
+                f.nice();
+            })`,
+            errors: [
+                { messageId: "amdNotAllowed" }
+            ],
+            output: [
+                "import f from \"funcs\";\n",
+                "f.nice();"
+            ].join("")
+        },
+        {
+            code: "define([], function() {console.log(\"test\");})",
+            errors: [
+                { messageId: "amdNotAllowed" }
+            ],
+            output: "console.log(\"test\");"
+        },
+        {
+            code: "define([], () => {console.log(\"test\");})",
+            errors: [
+                { messageId: "amdNotAllowed" }
+            ],
+            output: "console.log(\"test\");"
+        }
+    ]
+});
